0bef5756d8c1b40a523f4e55b449be087260dc1a
yywzt <2507967396@qq.com>
2019-03-29 00:04:48 +0800

    爬取网易云音乐 按歌单爬 按歌手爬

--- music163/spiders/music-discover2.py
+++ music163/spiders/music-discover2.py
@@ -7,7 +7,7 @@ from ..settings import DEFAULT_REQUEST_HEADERS
 # 爬取 https://music.163.com/#/discover/playlist/ 全部歌单
 
 class MusicDiscoverSpider(Spider):
-    name = "musicdiscover"
+    name = "musicdiscover2"
     allowed_domains = ["163.com"]
     base_url = 'https://music.163.com'
     # ids = ['1001','1002','1003','2001','2002','2003','6001','6002','6003','7001','7002','7003','4001','4002','4003']
@@ -21,18 +21,25 @@ class MusicDiscoverSpider(Spider):
     def start_requests(self):
         for id in self.ids:
             for initial in self.initials:
-                url = '{url}/discover/artist/cat?id={id}&initial={initial}'.format(url=self.base_url,id=id,initial=initial)
+                url = 'https://music.163.com/discover/playlist'.format(url=self.base_url,id=id,initial=initial)
                 yield Request(url, callback=self.parse_index)
 
     # 获得所有歌手的url
+    # def parse_index(self, response):
+    #     # print(response.text)
+    #     for sel in response.xpath('//ul[@id="m-pl-container"]//li//a[@class="msk"]/@href').extract():
+    #         print(sel)
+
+    # 全部分类的热门歌单
     def parse_index(self, response):
         # print(response.text)
-        for sel in response.xpath('//ul[@id="m-artist-box"]//li'):
-            artist=sel.re('href\=\"\/artist\?id\=[(0-9)]{4,9}')
-            for artistid in artist:
-                # print(artistid)
-                artist_url = self.base_url + '/artist?' + artistid[14:]
-                yield Request(artist_url,callback= self.parse_artist_pre)
+        for sel in response.xpath('//ul[@id="m-pl-container"]//li'):
+            artist=sel.xpath('./div/a[@class="msk"]/@href').extract()
+            titles=sel.xpath('./div/a[@class="msk"]/@title').extract()
+            imgs=sel.xpath('./div/img[@class="j-flag"]/@src').extract()
+            print(artist)
+            print(titles)
+            print(imgs)
 
     def parse_artist_pre(self,response):
         print(response)
